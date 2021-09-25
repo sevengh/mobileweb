348ead500c708b35e39370927e035da9b8a157f3
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2021-09-25 13:17:21 +0400

    re-implement splash

--- app/src/main/java/com/shamanayev/mobileweb/SplashActivity.kt
+++ app/src/main/java/com/shamanayev/mobileweb/SplashActivity.kt
@@ -10,11 +10,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 1500)
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
     }
 }
