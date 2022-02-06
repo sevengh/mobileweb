57d7e151c43574b8f9e59f38a5a0ada9dbf32b91
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2022-02-06 22:30:05 +0400

    Fixed "no internet" page on android version 32

--- app/src/main/java/com/shamanayev/mobileweb/MainActivity.kt
+++ app/src/main/java/com/shamanayev/mobileweb/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val url: String =
-            if (action === "com.shamanayev.mobileweb.Main")
+            if (action == "com.shamanayev.mobileweb.Main")
                 EnterUrlActivity.demoUrl
             else
                 sharedPreferences?.getString("url", "").toString()
